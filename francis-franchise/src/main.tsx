396ddbc5c78c80e4b3a763e43f11d311d1524864
EmanFrancis <emmanuel.o.b.francis@gmail.com>
2025-07-12 09:16:59 +0100

    Removed navigation and default readme.md

--- francis-franchise/src/main.tsx
+++ francis-franchise/src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Login from './login/login.tsx'
-import Navigation from './landing-page/Navigation.tsx'
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  <Navigation/>
+  
    <Login/>
   </StrictMode>,
 )
